39361f72d9367c297bc1cd5a597d6068222ad6db
thepacksin <bayare.deepak234@gmail.com>
2021-09-21 19:18:52 +0530

    API integration

--- app/src/main/java/com/prism/prismnews/MainActivity.kt
+++ app/src/main/java/com/prism/prismnews/MainActivity.kt
@@ -1,33 +1,73 @@
 package com.prism.prismnews
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), NewsItemClicked {
+class  MainActivity : AppCompatActivity(), NewsItemClicked {
+
+    private lateinit var mAdapter: NewsListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        val items = fetchdata()
-        val adapter:NewsListAdapter = NewsListAdapter(items, this)
-        recyclerView.adapter = adapter
+        fetchData()
+        mAdapter= NewsListAdapter(this)
+        recyclerView.adapter = mAdapter
 
 
     }
 
-    private fun fetchdata(): ArrayList<String> {
-        val list = ArrayList<String>()
-        for(i in 1 until 101){
-            list.add("Item $i")
+    private fun fetchData() {
+
+        val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=9edeb7c6b2574654828afb316dec47e0"
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<Newz>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = Newz(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNewz(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
         }
-        return list
+        // Access the RequestQueue through your singleton class.
+        NewzSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
-    override fun onItemClicked(item: String) {
-        Toast.makeText(this, "Clicked item is $item", Toast.LENGTH_LONG).show()
+    override fun onItemClicked(item: Newz) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabIntent = builder.build()
+        customTabIntent.launchUrl(this, Uri.parse(item.url));
     }
 
 }
